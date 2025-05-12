eca4d7c3647f2e329102d5b640926b27c3725311
Lucas Balieiro <github@lucasbalieiro.dev>
2025-05-12 17:18:12 -0400

    add sibling-io-communication example with server and client handlers

---
+++ examples/sibling-io-communication/src/configs.rs
@@ -0,0 +1,66 @@
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    str::FromStr,
+};
+
+use tower_stratum::{
+    client::service::config::{Sv2ClientServiceConfig, Sv2ClientServiceTemplateDistributionConfig},
+    key_utils::{Secp256k1PublicKey, Secp256k1SecretKey},
+    server::service::config::{
+        Sv2ServerServiceConfig, Sv2ServerServiceMiningConfig, Sv2ServerTcpConfig,
+    },
+};
+
+pub struct MyConfig {
+    pub server_config: Sv2ServerServiceConfig,
+    pub client_config: Sv2ClientServiceConfig,
+}
+
+impl MyConfig {
+    pub fn new() -> Self {
+        let server_config = Sv2ServerServiceConfig {
+            min_supported_version: 2,
+            max_supported_version: 2,
+            inactivity_limit: 3600,
+            tcp_config: Sv2ServerTcpConfig {
+                listen_address: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 3333),
+                pub_key: Secp256k1PublicKey::from_str(
+                    "9auqWEzQDVyd2oe1JVGFLMLHZtCo2FFqZwtKA5gd9xbuEu7PH72",
+                )
+                .unwrap(),
+                priv_key: Secp256k1SecretKey::from_str(
+                    "mkDLTBBRxdBv998612qipDYoTK3YUrqLe8uWw7gu3iXbSrn2n",
+                )
+                .unwrap(),
+                cert_validity: 3600,
+            },
+            mining_config: Some(Sv2ServerServiceMiningConfig {
+                supported_flags: 0b0101,
+            }),
+            job_declaration_config: None,
+            template_distribution_config: None,
+        };
+
+        let client_config = Sv2ClientServiceConfig {
+            min_supported_version: 2,
+            max_supported_version: 2,
+            endpoint_host: None,
+            endpoint_port: None,
+            vendor: None,
+            hardware_version: None,
+            device_id: None,
+            firmware: None,
+            mining_config: None,
+            job_declaration_config: None,
+            template_distribution_config: Some(Sv2ClientServiceTemplateDistributionConfig {
+                server_addr: SocketAddr::from_str("127.0.0.1:8442").unwrap(),
+                auth_pk: None,
+                coinbase_output_constraints: (1, 1),
+            }),
+        };
+        Self {
+            server_config,
+            client_config,
+        }
+    }
+}
---
+++ examples/sibling-io-communication/src/template_distribution_handler.rs
@@ -0,0 +1,87 @@
+use anyhow::Result;
+use roles_logic_sv2::template_distribution_sv2::{NewTemplate, SetNewPrevHash};
+
+use roles_logic_sv2::template_distribution_sv2::{
+    RequestTransactionDataError, RequestTransactionDataSuccess,
+};
+use std::task::{Context, Poll};
+use tower_stratum::client::service::request::{RequestToSv2Client, RequestToSv2ClientError};
+use tower_stratum::client::service::response::ResponseFromSv2Client;
+use tower_stratum::client::service::subprotocols::template_distribution::handler::Sv2TemplateDistributionClientHandler;
+use tower_stratum::server::service::request::RequestToSv2Server;
+use tower_stratum::server::service::subprotocols::mining::request::RequestToSv2MiningServer;
+use tracing::info;
+#[derive(Debug, Clone, Default)]
+pub struct MyTemplateDistributionHandler {
+    // Add fields here to store state or callbacks if needed.
+}
+
+/// Implements the `Sv2TemplateDistributionClientHandler` trait for `MyTemplateDistributionHandler`.
+/// This trait defines how the handler processes incoming template distribution events.
+impl Sv2TemplateDistributionClientHandler for MyTemplateDistributionHandler {
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), RequestToSv2ClientError>> {
+        // Indicates that the handler is ready to process requests.
+        Poll::Ready(Ok(()))
+    }
+
+    async fn handle_new_template(
+        &self,
+        template: NewTemplate<'static>,
+    ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
+        info!(
+            template_id = template.template_id,
+            "TDC: Forwarding new template to MiningServer"
+        );
+
+        // This is where the SiblingIO mechanism is utilized.
+        // The new template is forwarded to the MiningServer using a sibling request.
+        // For more details on SiblingIO, refer to the following link:
+        // https://github.com/plebhash/tower-stratum/blob/de6d909bae4c85e19b75b20081b921efb6830d81/src/client/service/mod.rs#L774-L781
+        // SiblingIO works by recursively triggering requests. In this case, a new request of type
+        // `RequestToSv2Client::SendRequestToSiblingServerService` is created and dispatched to the MiningServer.
+        // The MiningServer, being a sibling server of the TDC Handler, processes the request accordingly.
+
+        let response = ResponseFromSv2Client::TriggerNewRequest(
+            RequestToSv2Client::SendRequestToSiblingServerService(Box::new(
+                RequestToSv2Server::MiningTrigger(RequestToSv2MiningServer::NewTemplate(template)),
+            )),
+        );
+        Ok(response)
+    }
+
+    async fn handle_set_new_prev_hash(
+        &self,
+        prev_hash: SetNewPrevHash<'static>,
+    ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
+        info!(prev_hash = ?prev_hash, "MiningServer: Received new previous hash");
+
+        // Similar to `handle_new_template`, this forwards the new previous hash to the MiningServer.
+        let response = ResponseFromSv2Client::TriggerNewRequest(
+            RequestToSv2Client::SendRequestToSiblingServerService(Box::new(
+                RequestToSv2Server::MiningTrigger(RequestToSv2MiningServer::SetNewPrevHash(
+                    prev_hash,
+                )),
+            )),
+        );
+        Ok(response)
+    }
+
+    async fn handle_request_transaction_data_success(
+        &self,
+        transaction_data: RequestTransactionDataSuccess<'static>,
+    ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
+        info!(
+            "received request transaction data success: {:?}",
+            transaction_data
+        );
+        Ok(ResponseFromSv2Client::Ok)
+    }
+
+    async fn handle_request_transaction_data_error(
+        &self,
+        error: RequestTransactionDataError<'static>,
+    ) -> Result<ResponseFromSv2Client<'static>, RequestToSv2ClientError> {
+        info!("received request transaction data error: {:?}", error);
+        Ok(ResponseFromSv2Client::Ok)
+    }
+}
